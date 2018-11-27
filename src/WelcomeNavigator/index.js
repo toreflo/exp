158b277e86d16a9124844a676d0e4faa929cbecb
Salvatore De Persis <toreflo@MBPdiSalvatore.station>
2018-11-27 07:54:55 +0100

    Added custom header to Welcom stack navigator

--- src/WelcomeNavigator/index.js
+++ src/WelcomeNavigator/index.js
@@ -1,5 +1,6 @@
+import React from "react";
 import { createStackNavigator } from 'react-navigation';
-
+import { Header, Left, Right, Body, Title, Icon, Button } from 'native-base';
 import WelcomeScreen from './WelcomeScreen';
 import LoginScreen from './LoginScreen';
 import SignUpScreen from './SignUpScreen';
@@ -9,7 +10,24 @@ const WelcomeStackNavigator = createStackNavigator({
   LoginScreen: { screen: LoginScreen },
   SignUpScreen: { screen: SignUpScreen },
 }, {
-  // headerMode: 'none',
+  navigationOptions: ({ navigation }) => ({
+    header: (
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => navigation.goBack()}
+            >
+              <Icon type="Ionicons" name="ios-arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>{navigation.state.routeName}</Title>
+          </Body>
+          <Right/>
+        </Header>
+    ),
+  }),
 });
 
 export default WelcomeStackNavigator;
