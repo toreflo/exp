d49a8e90ddf5194e85ba2ddbd2ee42e98eef9485
Salvatore De Persis <EliteBook@NTU200241.it>
2019-02-13 14:33:10 +0100

    User interface changed. To be completed with group image and admin functions.

--- src/screens/GroupsScreen.js
+++ src/screens/GroupsScreen.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 import { 
   Container,
   Content,
   Fab,
-  Spinner,
   Text,
   Icon,
   Card,
   CardItem,
-  Body,
   Button,
-  Left,
   Right,
   Badge,
+  Grid,
+  Col,
+  Row,
 } from 'native-base';
 import firebase from 'firebase';
 import Dialog from 'react-native-dialog';
@@ -23,6 +23,18 @@ import 'moment/locale/it';
 
 import * as gbl from '../gbl';
 
+const getTimeRefs = () => {
+  const now = moment();
+  const today = now.clone().startOf('day');
+  const yesterday = now.clone().subtract(1, 'days').startOf('day');
+  const aWeekAgo = now.clone().subtract(7, 'days').startOf('day');
+
+  return { today, yesterday, aWeekAgo };
+}
+const isToday = (timestamp) => moment(timestamp).isSame(getTimeRefs().today, 'd');
+const isYesterday = (timestamp) => moment(timestamp).isSame(getTimeRefs().yesterday, 'd');
+const isWithinAWeek = (timestamp) => moment(timestamp).isAfter(getTimeRefs().aWeekAgo, 'd');
+
 class GroupsScreen extends Component {
   constructor(props) {
     super(props);
@@ -31,6 +43,7 @@ class GroupsScreen extends Component {
     this.createGroup = this.createGroup.bind(this);
     this.showGroupDialog = this.showGroupDialog.bind(this);
     this.hideGroupDialog = this.hideGroupDialog.bind(this);
+    this.renderMessage = this.renderMessage.bind(this);
   }
 
   componentDidMount() {
@@ -68,59 +81,84 @@ class GroupsScreen extends Component {
   hideGroupDialog() {
     this.setState({showAddGroup: false, newGroupName: ''}) 
   }
+  
+  getAdminSection = (data) => {
+    if (this.props.admin) {
+      return (
+        <CardItem style={{justifyContent: 'flex-end'}}>
+          <Right>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.navigate(
+                'GroupDetailsScreen',
+                { group: data },
+              )}
+            >
+              <Icon type="Ionicons" name="md-cog" style={{fontSize: 20}}/>
+            </Button>
+          </Right>
+        </CardItem>
+      );
+    }
+    return null;
+  }
+
+  renderMessage (data) {
+    const MAX_LEN = 25;
+    let info = null;
+    let lastMessageText;
+    const messages = this.props.messages[data.key];
+    let timeInfo;
+    if (messages && messages.length) {
+      const lastMessage = messages[messages.length - 1];
+      lastMessageText = lastMessage.text;
+      if (lastMessage.text.length > MAX_LEN) {
+        lastMessageText = lastMessage.text.substring(0, MAX_LEN) + '...'; 
+      }
+      if (isToday(lastMessage.createdAt)) timeInfo = moment(lastMessage.createdAt).format('LT');
+      else if (isYesterday(lastMessage.createdAt)) timeInfo = 'Ieri';
+      else if (isWithinAWeek(lastMessage.createdAt)) timeInfo = moment(lastMessage.createdAt).format('dddd');
+      else timeInfo = moment(lastMessage.createdAt).calendar();
+    }
+
+    return (
+      <Card style={{ borderRadius: 10, overflow: 'hidden' }}>
+        <CardItem
+          button
+          onPress={ () => this.goToDetails(data) }
+        >
+          <Image
+            key="avatar"
+            source={{uri: 'https://via.placeholder.com/80?text=group'}}
+            style={{
+              height: 50,
+              width: 50,
+              borderRadius: 50/2,
+            }}
+          />
+          <Grid style={{ marginLeft: 15 }}>
+            <Row>
+              <Col><Text style={{ fontSize: 15, fontWeight: 'bold' }}>{data.name}</Text></Col>
+              <Col><Text style={{ textAlign: 'right', color: '#959595' }}>{timeInfo}</Text></Col>
+            </Row>
+            <Row>
+              <Text style={{ marginTop: 12, color: '#959595' }}>{lastMessageText}</Text>
+            </Row>
+          </Grid>
+          {info}
+        </CardItem>
+      </Card>
+    );
+  }
 
   render() {
-    const getAdminSection = (data) => {
-      if (this.props.admin) {
-        return (
-          <CardItem style={{justifyContent: 'flex-end'}}>
-            <Right>
-              <Button
-                transparent
-                onPress={() => this.props.navigation.navigate(
-                  'GroupDetailsScreen',
-                  { group: data },
-                )}
-              >
-                <Icon type="Ionicons" name="md-cog" style={{fontSize: 20}}/>
-              </Button>
-            </Right>
-          </CardItem>
-        );
-      }
-      return null;
-    } 
     const content = (
       <Content>
         <FlatList
           style={{ padding: 15, paddingBottom: 75 }}
           enableEmptySections
           data={this.props.groups.sort(this.sortGroups)}
-
-          renderItem={({ item: data }) => {
-            let info = null;
-            let right = null;
-            if (!this.props.admin) {
-              const userInfo = this.props.userGroups[data.key];
-              info = <Text> {`${moment.unix(userInfo.lastMessageRead/1000).format('LLL')}`} </Text>;
-              if (userInfo.unread) right = (<Right><Badge><Text>{userInfo.unread}</Text></Badge></Right>);
-            }
-            return (
-              <Card style={{ borderRadius: 10, overflow: 'hidden' }}>
-                <CardItem
-                  button
-                  onPress={ () => this.goToDetails(data) }
-                >
-                  <Body>
-                    <Text> {data.name} </Text>
-                    {info}
-                  </Body>
-                  {right}
-                </CardItem>
-                {getAdminSection(data)}
-              </Card>
-            );
-          }}
+          renderItem={({ item: data }) => this.renderMessage(data)}
         />
       </Content>
     );
@@ -163,10 +201,19 @@ class GroupsScreen extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  admin: state.info.admin,
-  groups: state.groups,
-  userGroups: state.info.admin ? undefined : state.users.find(user => user.key === state.info.uid).groups,
-});
+const mapStateToProps = (state) => {
+  const userGroups = state.info.admin ? undefined :
+    state.users.find(user => user.key === state.info.uid).groups;
+  const groupKeys = state.info.admin ?
+    state.groups.map(group => group.key) :
+    Object.keys(userGroups);
+
+  return {
+    admin: state.info.admin,
+    groups: state.groups,
+    userGroups,
+    messages: state.groupMessages,
+  };
+};
 
 export default connect(mapStateToProps)(GroupsScreen);
